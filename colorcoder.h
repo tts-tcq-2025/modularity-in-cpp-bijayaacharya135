af96188a2eae567b90f2e722baf8e38a21e87c97
bijayaacharya135 <internbijaya@gmail.com>
2025-08-12 08:39:40 +0530

    colorcoder.h

--- colorcoder.h
+++ colorcoder.h
@@ -1,22 +1,29 @@
 #pragma once
+#include <string>
+#include <vector>
 
 namespace TelCoColorCoder
 {
-    enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-    enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+    enum class MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET, COUNT};
+    enum class MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE, COUNT};
 
-    class ColorPair {
-    public:
-        ColorPair(MajorColor major, MinorColor minor);
-        MajorColor getMajorColor() const;
-        MinorColor getMinorColor() const;
+    struct ColorPair {
+        MajorColor major;
+        MinorColor minor;
+        ColorPair(MajorColor maj, MinorColor min) : major(maj), minor(min) {}
         std::string ToString() const;
-    private:
-        MajorColor majorColor;
-        MinorColor minorColor;
     };
 
-    ColorPair GetColorFromPairNumber(int pairNumber);
     int GetPairNumberFromColor(MajorColor major, MinorColor minor);
-    std::string GetReferenceManual();
+    ColorPair GetColorFromPairNumber(int pairNumber);
+
+    std::string MajorColorToString(MajorColor major);
+    std::string MinorColorToString(MinorColor minor);
+
+    struct ReferenceEntry {
+        int pairNumber;
+        MajorColor major;
+        MinorColor minor;
+    };
+    std::vector<ReferenceEntry> GetReferenceManual();
 }
